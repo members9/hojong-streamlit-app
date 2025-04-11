17bfd6366bb81a8e418f85793a74064a4ca95afc
members9 <members9@gmail.com>
2025-04-12 01:49:20 +0900

    Update hojong_streamlit.py

--- hojong_streamlit.py
+++ hojong_streamlit.py
@@ -75,7 +75,7 @@ st.markdown("""
             overflow: hidden !important;
             width: 100% !important;
             padding-bottom: 2px !important;
-            margin-top: -24px !important;
+            margin-top: -100px !important;
         }
         .responsive-subtitle {
             font-size: clamp(14px, 5vw, 12px) !important;
